3e80783a12430e8c87d61e3b63ac31fc0478c9b3
Alex Molnar <alex.molnar@ing.com>
2026-08-06 11:07:24 +0200

    Initial commit add Dockerfile for Fast API

---
+++ api/app/main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+
+from .db.connection import connect
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: str | None = None):
+    return {"item_id": item_id, "q": q}
