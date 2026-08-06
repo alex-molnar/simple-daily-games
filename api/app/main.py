4ac9c01fd67310e4dbb179dcfbda0dd12d22039a
Alex Molnar <alex.molnar@ing.com>
2026-08-06 21:35:45 +0200

    Refactored request models

--- api/app/main.py
+++ api/app/main.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI, Response # pyright: ignore[reportMissingImports]
 
 from .db.implementations import save_new_user, update_start, update_failed, update_success
-from .model import NewUserRequest, RegisterScoreRequest
 
 
 app = FastAPI()
@@ -14,22 +13,22 @@ def wrap(data, endpoint: str, method: str, response: Response, status_code: int 
     else:
         return data
 
-@app.post("/users/register", status_code=201)
-def register_user(data: NewUserRequest, response: Response):
-    return wrap(save_new_user(data.gameId), endpoint="/users/register", method="POST", response=response)
+@app.post("/games/{gameId}/users/register", status_code=201)
+def register_user(gameId: str, response: Response):
+    return wrap(save_new_user(gameId), endpoint="/games/<gameId>/users/register", method="POST", response=response)
 
-@app.put("/users/start_game", status_code=200)
-def start_game(data: RegisterScoreRequest, response: Response):
-    return wrap(update_start(data.userId, data.gameId), endpoint="/users/start_game", method="PUT", response=response)
+@app.post("/games/{gameId}/users/{userId}/start_game", status_code=200)
+def start_game(gameId: str, userId: str, response: Response):
+    return wrap(update_start(userId, gameId), endpoint="/games/<gameId>/users/<userId>/start_game", method="POST", response=response)
 
-@app.put("/users/failed_game", status_code=200)
-def failed_game(data: RegisterScoreRequest, response: Response):
-    return wrap(update_failed(data.userId, data.gameId), endpoint="/users/failed_game", method="PUT", response=response)
+@app.post("/games/{gameId}/users/{userId}/failed_game", status_code=200)
+def failed_game(gameId: str, userId: str, response: Response):
+    return wrap(update_failed(userId, gameId), endpoint="/games/<gameId>/users/<userId>/failed_game", method="POST", response=response)
 
-@app.put("/users/success_game", status_code=200)
-def success_game(data: RegisterScoreRequest, response: Response):
+@app.post("/games/{gameId}/users/{userId}/success_game/{attempts}", status_code=200)
+def success_game(gameId: str, userId: str, attempts: int, response: Response = None):
     return (
-        wrap(update_success(data.userId, data.gameId, data.attempts), endpoint="/users/success_game", method="PUT", response=response)
-        if data.attempts is not None and data.attempts > 0
-        else wrap("Attempts must be provided and greater than 0 for success_game endpoint", endpoint="/users/success_game", method="PUT", response=response, status_code=400)
+        wrap(update_success(userId, gameId, attempts), endpoint="/games/<gameId>/users/<userId>/success_game/<attempts>", method="POST", response=response)
+        if attempts > 0
+        else wrap("Attempts must be greater than 0 for success_game endpoint", endpoint="/games/<gameId>/users/<userId>/success_game/<attempts>", method="POST", response=response, status_code=400)
     )
