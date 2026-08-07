6603064c97e1ebf45eaeee6611f519b15648109b
Alex Molnar <alex.molnar@ing.com>
2026-08-07 12:56:08 +0200

    Switched to environment based config loading to support easier deployment

--- api/app/db/config.py
+++ api/app/db/config.py
@@ -1,20 +1,15 @@
-from configparser import ConfigParser
+import os
 
-def load_config(filename='/code/db/database.ini', section='postgresql'):
-    parser = ConfigParser()
-    parser.read(filename)
-
-    # get section, default to postgresql
-    config = {}
-    if parser.has_section(section):
-        params = parser.items(section)
-        for param in params:
-            config[param[0]] = param[1]
-    else:
-        print(parser.sections())
-        raise Exception('Section {0} not found in the {1} file'.format(section, filename))
-
+def load_config():
+    config = {
+        'host': os.getenv('POSTGRES_HOSTNAME', 'host.docker.internal'),
+        'database': os.getenv('POSTGRES_DB', 'simple-daily-games'),
+        'user': os.getenv('POSTGRES_USER', 'simple-daily-games'),
+        'password': os.getenv('POSTGRES_PASSWORD', 'password'),
+        'port': os.getenv('POSTGRES_PORT', 5432)
+    }
     return config
+
 
 if __name__ == '__main__':
     config = load_config()
