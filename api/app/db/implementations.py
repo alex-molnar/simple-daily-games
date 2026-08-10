65acf3570e9e0e395e08ddfae97a1f11b1c436b9
Alex Molnar <alex.molnar@ing.com>
2026-08-10 08:27:33 +0200

    Added proper logging to api

--- api/app/db/implementations.py
+++ api/app/db/implementations.py
@@ -1,24 +1,31 @@
+from logging import getLogger
+
 from psycopg2 import connect # pyright: ignore[reportMissingModuleSource, reportMissingImports]
 from datetime import datetime, timedelta
 
 from .config import load_config
 
 
+log = getLogger(__name__)
+
+
 def test_connection() -> dict | str:
     config = load_config()
+    log.debug(f'Loaded database configuration: {config}')
     try:
         with connect(**config) as _:
-            print('Connected to the PostgreSQL server.')
+            log.info('Connected to the PostgreSQL server.')
             return {
                 'application': 'up',
                 'db': 'up'
             }
     except Exception as e:
-        print(f'Error connecting to the PostgreSQL server: {e}')
+        log.error(f'Error connecting to the PostgreSQL server: {e}')
         return "Couldn't connect to the PostgreSQL server"
 
 def save_new_game(game_id: str) -> dict | str:
     config = load_config()
+    log.debug(f'Loaded database configuration: {config}')
     try:
         with connect(**config) as conn:
             with conn.cursor() as cur:
@@ -27,32 +34,40 @@ def save_new_game(game_id: str) -> dict | str:
                 gameIds = [game_id] * 365
                 dates = [(now + timedelta(days=day)).strftime("%Y-%m-%d") for day in range(365)]
                 cur.execute(query, tuple([item for pair in zip(dates, gameIds) for item in pair]))
+                log.debug(f'Executed query: {query} with parameters: {list(zip(dates, gameIds))}')
                 conn.commit()
-                print(f'Inserted rows for a year  game {game_id}')
+                log.info(f'Inserted rows for a year  game {game_id}')
                 return {
                     'gameId': game_id,
                     "message": "Game dates saved for the next 365 days"
                 }
     except Exception as e:
-        print(f'Error inserting new game: {e}')
+        log.error(f'Error inserting new game: {e}')
         return str(e)
 
 def _update_template(date: str, game_id: str, field: str) -> dict | str:
     config = load_config()
+    log.debug(f'Loaded database configuration: {config}')
     try:
         with connect(**config) as conn:
             with conn.cursor() as cur:
-                cur.execute(f"UPDATE results SET {field} = {field} + 1 WHERE date = %s AND gameId = %s RETURNING {field}", (date, game_id))
-                value = cur.fetchone()[0]
+                query = f"UPDATE results SET {field} = {field} + 1 WHERE date = %s AND gameId = %s RETURNING {field}"
+                cur.execute(query, (date, game_id))
+                log.debug(f'Executed query: {query} with parameters: {(date, game_id)}')
+                value = cur.fetchone()
                 conn.commit()
-                print(f'Updated {field} count for user: {date} and game: {game_id}')
-                return {
-                    'date': date,
-                    'gameId': game_id,
-                    field: value
-                }
+                if value:
+                    log.debug(f'Updated {field} count for game {game_id} on date {date}: {value[0]}')
+                    return {
+                        'date': date,
+                        'gameId': game_id,
+                        field: value[0]
+                    }
+                else:
+                    log.warning(f'No rows updated for game {game_id} on date {date}. Check if the gameId and date exist.')
+                    return f"No rows updated for gameId: {game_id} on date: {date}. Check if the gameId and date exist."
     except Exception as e:
-        print(f'Error updating {field} count: {e}')
+        log.error(f'Error updating {field} count: {e}')
         return str(e)
 
 def update_start(date: str, game_id: str) -> dict | str:
@@ -72,19 +87,23 @@ def get_stats_by(**kwargs) -> dict | str:
     if not kwargs:
         return "No parameters provided for stats retrieval"
     config = load_config()
+    log.debug(f'Loaded database configuration: {config}')
     try:
         with connect(**config) as conn:
             with conn.cursor() as cur:
                 query = "SELECT * FROM results WHERE " + ' AND '.join([f"{key} = %s" for key in kwargs.keys()])
                 cur.execute(query, tuple(kwargs.values()))
+                log.debug(f'Executed query: {query} with parameters: {tuple(kwargs.values())}')
                 result = cur.fetchone()
                 if result:
+                    log.info(f'Fetched results for parameters: {kwargs}')
                     columns = [desc[0] for desc in cur.description]
                     return dict(zip(columns, result))
                 else:
+                    log.warning(f'No results found for parameters: {kwargs}')
                     return f"No results found for {kwargs}"
     except Exception as e:
-        print(f'Error retrieving stats: {e}')
+        log.error(f'Error retrieving stats: {e}')
         return str(e)
 
 def get_stats_by_game_and_date(game_id: str, date: str) -> dict | str:
--- api/app/main.py
+++ api/app/main.py
@@ -1,6 +1,8 @@
 from datetime import datetime
 from fastapi import FastAPI, Response # pyright: ignore[reportMissingImports]
 from fastapi.middleware.cors import CORSMiddleware # pyright: ignore[reportMissingImports]
+from logging import basicConfig, getLogger, INFO
+from os import getenv
 
 from .db.implementations import test_connection, save_new_game, update_start, update_failed, update_success, get_stats_by_game_and_date
 
@@ -16,53 +18,74 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+log = getLogger(__name__)
+
+basicConfig(
+    level=getenv("LOG_LEVEL", INFO),
+    format="%(asctime)s [%(name)s] %(levelname)s: %(message)s",
+)
+
 
 def wrap(data, endpoint: str, method: str, response: Response, status_code: int = 500):
     if method == "GET":
         response.headers["Content-Type"] = "application/json"
     if type(data) == str:
         response.status_code = status_code
+        log.debug(f'Responding with error for {method} {endpoint}: {data}')
         return {"error": data, "endpoint": endpoint, "method": method}
     else:
+        log.debug(f'Responding with data for {method} {endpoint}: {data}')
         return data
+
+def log_request(endpoint: str, method: str, kwargs: dict = {}, data = None):
+    log.debug(f"Received request for {method} {endpoint} with parameters: {kwargs} and body: {data}")
 
 
 @app.get("/health", status_code=200)
 def health_check(response: Response):
+    log_request(endpoint="/health", method="GET")
     return wrap({'status': 'up'}, endpoint="/health", method="GET", response=response)
 
 @app.get("/readiness", status_code=200)
 def readiness_check(response: Response):
+    log_request(endpoint="/readiness", method="GET")
     return wrap(test_connection(), endpoint="/readiness", method="GET", response=response, status_code=503)
 
 @app.post("/games/register/{gameId}", status_code=201)
 def register_user(gameId: str, response: Response):
+    log_request(endpoint="/games/register/<gameId>", method="POST", kwargs={"gameId": gameId})
     return wrap(save_new_game(gameId), endpoint="/games/register/<gameId>", method="POST", response=response)
 
 @app.post("/games/{gameId}/extend", status_code=201)
-def register_user(gameId: str, response: Response):
+def extend_game(gameId: str, response: Response):
+    log_request(endpoint="/games/<gameId>/extend", method="POST", kwargs={"gameId": gameId})
     return wrap(save_new_game(gameId), endpoint="/games/<gameId>/extend", method="POST", response=response)
 
 @app.post("/games/{gameId}/date/{date}/start_game", status_code=200)
 def start_game(gameId: str, date: str, response: Response):
+    log_request(endpoint="/games/<gameId>/date/<date>/start_game", method="POST", kwargs={"gameId": gameId, "date": date})
     return wrap(update_start(date, gameId), endpoint="/games/<gameId>/date/<date>/start_game", method="POST", response=response)
 
 @app.post("/games/{gameId}/date/today/start_game", status_code=200)
 def start_game_today(gameId: str, response: Response):
     today = datetime.now().strftime("%Y-%m-%d")
+    log_request(endpoint="/games/<gameId>/date/today/start_game", method="POST", kwargs={"gameId": gameId, "date": today})
     return wrap(update_start(today, gameId), endpoint="/games/<gameId>/date/today/start_game", method="POST", response=response)
 
 @app.post("/games/{gameId}/date/{date}/failed_game", status_code=200)
 def failed_game(gameId: str, date: str, response: Response):
+    log_request(endpoint="/games/<gameId>/date/<date>/failed_game", method="POST", kwargs={"gameId": gameId, "date": date})
     return wrap(update_failed(date, gameId), endpoint="/games/<gameId>/date/<date>/failed_game", method="POST", response=response)
 
 @app.post("/games/{gameId}/date/today/failed_game", status_code=200)
 def failed_game_today(gameId: str, response: Response):
     today = datetime.now().strftime("%Y-%m-%d")
+    log_request(endpoint="/games/<gameId>/date/today/failed_game", method="POST", kwargs={"gameId": gameId, "date": today})
     return wrap(update_failed(today, gameId), endpoint="/games/<gameId>/date/today/failed_game", method="POST", response=response)
 
 @app.post("/games/{gameId}/date/{date}/success_game/{attempts}", status_code=200)
 def success_game(gameId: str, date: str, attempts: int, response: Response = None):
+    log_request(endpoint="/games/<gameId>/date/<date>/success_game/<attempts>", method="POST", kwargs={"gameId": gameId, "date": date, "attempts": attempts})
     return (
         wrap(update_success(date, gameId, attempts), endpoint="/games/<gameId>/date/<date>/success_game/<attempts>", method="POST", response=response)
         if attempts > 0
@@ -72,6 +95,7 @@ def success_game(gameId: str, date: str, attempts: int, response: Response = Non
 @app.post("/games/{gameId}/date/today/success_game/{attempts}", status_code=200)
 def success_game_today(gameId: str, attempts: int, response: Response = None):
     today = datetime.now().strftime("%Y-%m-%d")
+    log_request(endpoint="/games/<gameId>/date/today/success_game/<attempts>", method="POST", kwargs={"gameId": gameId, "attempts": attempts, "date": today})
     return (
         wrap(update_success(today, gameId, attempts), endpoint="/games/<gameId>/date/today/success_game/<attempts>", method="POST", response=response)
         if attempts > 0
@@ -80,8 +104,10 @@ def success_game_today(gameId: str, attempts: int, response: Response = None):
 
 @app.get("/games/{gameId}/date/{date}/stats", status_code=200)
 def get_user_stats_endpoint(gameId: str, date: str, response: Response):
+    log_request(endpoint="/games/<gameId>/date/<date>/stats", method="GET", kwargs={"gameId": gameId, "date": date})
     return wrap(get_stats_by_game_and_date(gameId, date), endpoint="/games/<gameId>/date/<date>/stats", method="GET", response=response)
 
 @app.get("/games/{gameId}/today", status_code=200)
 def get_today_game(gameId: str, response: Response):
+    log_request(endpoint="/games/<gameId>/today", method="GET", kwargs={"gameId": gameId, "date": datetime.now().strftime("%Y-%m-%d")})
     return wrap(get_stats_by_game_and_date(gameId, datetime.now().strftime("%Y-%m-%d")), endpoint="/games/<gameId>/today", method="GET", response=response)
